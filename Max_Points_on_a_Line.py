b45da9bb4eb641e2900082218e4d1659e1c74c6a
WolkeDu <duf3434@gmail.com>
2014-02-22 16:59:33 +0800

    none

--- Max_Points_on_a_Line.py
+++ Max_Points_on_a_Line.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python
+# Definition for a point
+# class Point:
+#     def __init__(self, a=0, b=0):
+#         self.x = a
+#         self.y = b
+
+class Solution:
+    # @param points, a list of Points
+    # @return an integer
+    def maxPoints(self, points):
+        if len(points) == 0:
+            return 0
+        dictg = {}
+        
